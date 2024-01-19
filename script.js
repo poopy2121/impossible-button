18f7fd34ee8ac706cbfec97bc5faa4e164d571d2
poopy2121 <ggg1111g1g1g@gmail.com>
2024-01-19 20:36:05 +0100

    created button sound function

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("btn");
+const btnsound = new Audio('sounds/mixkit-arcade-game-jump-coin-216.wav');
+
+
+button.addEventListener('click', function() {
+
+    btnsound.play();
+    
+})
